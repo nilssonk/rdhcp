3ef23a1d31209ddc4895a4c6ef24c0cdf84a7127
Kim Nilsson <kim@wayoftao.net>
2022-09-16 23:10:18 +0200

    Update DhcpPacket implementation
    * Use views
    * Add mutable accessors
    * Update tests to reflect API change

--- src/common.rs
+++ src/common.rs
@@ -1,11 +1,18 @@
-mod be_integer;
-use be_integer::BeInteger;
-
+#[macro_use]
 mod integer_view;
+pub(crate) use integer_view::integer_view;
+pub(crate) use integer_view::integer_view_mut;
 pub use integer_view::IntegerView;
 
+#[macro_use]
 mod ipv4_addr_view;
+pub(crate) use ipv4_addr_view::ipv4_addr_view;
+pub(crate) use ipv4_addr_view::ipv4_addr_view_mut;
 pub use ipv4_addr_view::Ipv4AddrView;
 
+// Traits
+mod be_integer;
+pub use be_integer::BeInteger;
+
 mod sized_integer;
-use sized_integer::SizedInteger;
+pub(crate) use sized_integer::SizedInteger;
