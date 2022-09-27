beab6e73bb121a6c0f09841b75040b09c7d67227
Kim Nilsson <92694405+nilssonk@users.noreply.github.com>
2022-09-27 22:59:17 +0200

    Add BeInteger trait (#26)

---
+++ src/common/be_integer.rs
@@ -0,0 +1,27 @@
+use crate::common::SizedInteger;
+
+pub trait BeInteger {
+    fn read_be(source: &[u8]) -> Self;
+    fn write_be(&self, destination: &mut [u8]);
+}
+
+macro_rules! make_impl {
+    {$t:ty} => {
+    impl BeInteger for $t {
+        fn read_be(source: & [u8]) -> Self {
+            const SIZE: usize = <$t as SizedInteger>::SIZE;
+            Self::from_be_bytes(source[..SIZE].try_into().unwrap())
+        }
+
+        fn write_be(&self, destination: &mut [u8]) {
+            let data = self.to_be_bytes();
+            destination.copy_from_slice(&data);
+        }
+    }
+    };
+}
+
+make_impl! {u8}
+make_impl! {u16}
+make_impl! {u32}
+make_impl! {u64}
