8bf1938f9567d0e46d67360822e7275d53b8a84b
Kim Nilsson <92694405+nilssonk@users.noreply.github.com>
2022-09-29 21:17:39 +0200

    Add Ipv4AddrView (#28)

--- src/common.rs
+++ src/common.rs
@@ -4,5 +4,8 @@ use be_integer::BeInteger;
 mod integer_view;
 pub use integer_view::IntegerView;
 
+mod ipv4_addr_view;
+pub use ipv4_addr_view::Ipv4AddrView;
+
 mod sized_integer;
 use sized_integer::SizedInteger;
